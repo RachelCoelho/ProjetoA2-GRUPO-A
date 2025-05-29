084565d8e526642aedeaad45c4e643dfd4750681
RachelCoelho <163034541+RachelCoelho@users.noreply.github.com>
2025-05-29 10:49:54 -0300

    Update Conversao.java

--- ProjetoA2/Conversao.java
+++ ProjetoA2/Conversao.java
@@ -1,7 +1,15 @@
-package temperatura;
+package temperatura; // Define o pacote onde a interface está localizada
 
+// A interface Conversao define os métodos que uma classe de temperatura deve implementar
+// para permitir a conversão entre diferentes escalas de temperatura.
 public interface Conversao {
+
+    // Método para converter uma temperatura para Celsius
     double converterParaCelsius();
+
+    // Método para converter uma temperatura para Fahrenheit
     double converterParaFahrenheit();
+
+    // Método para converter uma temperatura para Kelvin
     double converterParaKelvin();
 }
